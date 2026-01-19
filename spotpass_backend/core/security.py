7d15c624841d9ad5f40cee9ad9cb20cc875c4574
Mounir Messelmeni <messelmeni.mounir@gmail.com>
2026-01-19 14:52:58 +0100

    Enhance password hashing support by adding argon2 and updating dependencies

--- spotpass_backend/core/security.py
+++ spotpass_backend/core/security.py
@@ -9,7 +9,7 @@ from passlib.context import CryptContext
 from core.config import settings
 
 # Password hashing context
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["bcrypt", "argon2", "pbkdf2_sha256"], deprecated="auto")
 
 
 def hash_password(password: str) -> str:
