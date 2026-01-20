2b3fb7022b3c3a82538c5244bc9b011d4c4c935c
Mounir Messelmeni <messelmeni.mounir@gmail.com>
2026-01-20 23:09:44 +0100

    Add @types/node dependency and remove vite.config.js

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,4 +22,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
